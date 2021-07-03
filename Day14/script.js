56803afe8de4eb72ee04a33156fec141efbec26d
Rex-Arnab <khanp4397@gmail.com>
2021-07-03 20:54:02 +0530

    Completed THA14

---
+++ Day14/script.js
@@ -0,0 +1,45 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const currentTime = [];
+
+currentTime.unshift(getTimestamp());
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomKey() {
+  return keys[getRandomNumber(0, keys.length-1)]
+}
+
+function targetRandomKey() {
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+  let start = Date.now()
+}
+
+function getTimestamp() {
+  return Math.floor(Date.now() / 1000)
+}
+
+document.addEventListener("keyup", event => {
+  const keyPressed = String.fromCharCode(event.keyCode);
+  const keyElement = document.getElementById(keyPressed);
+  const highlightedKey = document.querySelector(".selected");
+  
+  keyElement.classList.add("hit")
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+  })
+  
+  if (keyPressed === highlightedKey.innerHTML) {
+    currentTime.unshift(getTimestamp());
+    const elapsedTime = currentTime[0] - currentTime[1];
+    subtitle.innerHTML =`Character per minute ${60/elapsedTime}`
+    highlightedKey.classList.remove("selected");
+    targetRandomKey();
+  } 
+})
+
+targetRandomKey();
