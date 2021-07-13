08453147da03035fb282695263c71ce2400f2a5a
Arnab Biswas <khanp4397@gmail.com>
2021-07-13 18:50:53 +0530

    fixing Bug in THA21

--- Day21/src/App.js
+++ Day21/src/App.js
@@ -1,85 +1,7 @@
 import "./styles.css";
-import { useState, useEffect } from "react";
-
-const ShowAddPanel = ({ handleAddTodo, cals, setCals, val, setVal }) => {
-  return (
-    <form className="panel" onSubmit={handleAddTodo}>
-      <input
-        type="text"
-        value={cals}
-        onChange={(e) => setCals(e.target.value)}
-        placeholder="Enter Food Name"
-        className="inputOne"
-      />
-      <input
-        type="number"
-        value={val}
-        onChange={(e) => setVal(e.target.value)}
-        placeholder="Enter Calori Amount"
-        className="inputTwo"
-      />
-      <button className="BtnOne">Add</button>
-    </form>
-  );
-};
-
-const TodoItem = ({ ca, va }) => {
-  const [switchDisplay, setDisplay] = useState(true);
-  const [cals, setCals] = useState(ca);
-  const [val, setVal] = useState(va);
-  const [isDeleted, setDeleted] = useState(true);
-  return isDeleted ? (
-    <div className="list">
-      {switchDisplay ? (
-        <>
-          <div className="title">
-            <span>{cals}</span>
-          </div>
-          <div className="desc">
-            <span>You have consumed {val} calories</span>
-          </div>
-        </>
-      ) : (
-        <>
-          <input value={cals} onChange={(e) => setCals(e.target.value)} />
-          <input value={val} onChange={(e) => setVal(e.target.value)} />
-        </>
-      )}
-
-      <div className="actionPanel">
-        <button
-          onClick={() => {
-            setDeleted(false);
-          }}
-        >
-          Delete
-        </button>
-        <button
-          onClick={() => {
-            console.log(switchDisplay);
-            setDisplay(switchDisplay ? false : true);
-          }}
-        >
-          {switchDisplay ? "Edit" : "Done"}
-        </button>
-      </div>
-    </div>
-  ) : null;
-};
-const ShowTodoList = ({ todos }) => {
-  const [itemCount, setItemCount] = useState(0);
-  return (
-    <div className="TodoList">
-      {itemCount ? (
-        <div className="alert white">Start Adding Item</div>
-      ) : (
-        todos.map((todo, index) => {
-          return <TodoItem ca={todo[0]} va={todo[1]} key={index} />;
-        })
-      )}
-    </div>
-  );
-};
+import { useState } from "react";
+import ShowAddPanel from "./components/ShowAddPanel";
+import ShowTodoList from "./components/ShowTodoList";
 
 export default function App() {
   const [cals, setCals] = useState("");
@@ -91,7 +13,8 @@ export default function App() {
     setTodo(() => {
       return [...todo, [cals, val]];
     });
-    console.log(todo);
+    setCals("");
+    setVal(0);
   };
   return (
     <div className="App">
---
+++ Day21/src/components/ShowTodoList.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+const ShowTodoList = ({ todos }) => {
+  const [itemCount, setItemCount] = useState(0);
+  return (
+    <div className="TodoList">
+      {itemCount ? (
+        <div className="alert white">Start Adding Item</div>
+      ) : (
+        todos.map((todo, index) => {
+          return <TodoItem ca={todo[0]} va={todo[1]} key={index} />;
+        })
+      )}
+    </div>
+  );
+};
+
+export default ShowTodoList;
