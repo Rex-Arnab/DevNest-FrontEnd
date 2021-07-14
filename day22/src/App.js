8472bc8c31e90720159d673f462560ba6c544fde
Arnab Biswas <khanp4397@gmail.com>
2021-07-14 18:52:35 +0530

    Completed THA 22

---
+++ day22/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import {Memes, Meme} from "./components/Memes";
+
+export default function App() {
+  const [memes, setMemes] = useState([]);
+  const [meme, setMeme] = useState(null);
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.data.memes);
+        setMemes(data.data.memes);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Meme Generator</h1>
+      {meme ? <Meme meme={meme} setMeme={setMeme}/> : <Memes memes={memes} setMeme={setMeme} />}
+    </div>
+  );
+}
