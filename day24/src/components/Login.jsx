3595b569006768462de99fe9d2bba087eedc9fe1
Arnab Biswas <khanp4397@gmail.com>
2021-07-30 00:43:07 +0530

    Pushing THA24 build

--- day24/src/components/Login.jsx
+++ day24/src/components/Login.jsx
@@ -14,32 +14,20 @@ export default function Login(){
         Sign in to your account
       </h2>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
-      <input type="hidden" name="remember" value="true" />
-      <div class="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label for="email-address" class="sr-only">Email address</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
-        </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
-        </div>
-      </div>
-
+    <div class="mt-8 space-y-6">
       <div>
         <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white" onClick={() => {
-          setlogedin(true)
+          isLoggedIn ? setlogedin(false) : setlogedin(true)
         }}>
           <span class="absolute left-0 inset-y-0 flex items-center pl-3">
             <svg class="h-5 w-5 text-black hover:text-white group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill='white' aria-hidden="true">
               <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
             </svg>
           </span>
-          Sign in
+          {isLoggedIn ? 'Sign Out' : 'Sign in'}
         </button>
       </div>
-    </form>
+    </div>
   </div>
 </div>
 
