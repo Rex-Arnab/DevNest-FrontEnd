b80504cf1c92786b9380eb01628360424e9e4e1c
Arnab Biswas <khanp4397@gmail.com>
2021-08-02 17:31:04 +0530

    Uploading THA26

--- day26/src/App.js
+++ day26/src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
-import AddTodoPanel from './components/AddTodoPanel';
-import DisplayTodoList from './components/DisplayTodoList';
 
 function App() {
   return (
     <div className="App">
-      {/* <AddTodoPanel /> */}
-      {/* <DisplayTodoList /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          From DarkCookie
+        </p>
+      </header>
     </div>
   );
 }
