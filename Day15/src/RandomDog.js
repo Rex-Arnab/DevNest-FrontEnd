39b2d04f4579c1b4bb82591dfc53fc83f49cdbf8
Arnab Biswas <khanp4397@gmail.com>
2021-09-13 23:46:15 +0530

    Optimizing Fetch Time

--- Day15/src/RandomDog.js
+++ Day15/src/RandomDog.js
@@ -4,13 +4,13 @@ function RandomCat(){
 	const [newCat, setNewCat] = useState('https://purr.objects-us-east-1.dream.io/i/0oKOv.jpg')
 	const [ext, setExt] = useState('')
 	useEffect(() => {
-		fetch('https://csb-619dg-lkypw81z0-rex-arnab.vercel.app/api/randomDog.js').then(resp => resp.json())
+		fetch('https://csb-619dg-6xc9jhhpf-rex-arnab.vercel.app/api/randomDog.js').then(resp => resp.json())
 		.then(data => {
 			setNewCat(data.url)
 			setExt(data.ext)
 		})
 	},[])
-	return (ext === ".mp4") ? <video src={newCat} controls /> : <img src={newCat} alt='new cat' />;
+	return (ext === ".mp4" || ext === ".webm") ? <video src={newCat} controls /> : <img src={newCat} alt='new cat' />;
 }
 
 export default RandomCat;
