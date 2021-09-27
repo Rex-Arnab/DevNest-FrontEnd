df570c9047d0c9e462224e6c000db6df9bc30d06
Arnab Biswas <khanp4397@gmail.com>
2021-09-27 18:30:48 +0530

    Updating Instagram Clone

--- day35/src/App.tsx
+++ day35/src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
-import Navbar from './components/Navbar'
+import React, { useEffect, useState } from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap"
+import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "./redux/actions/userActions";
+import Landing from "./components/Landing";
+
+export interface IPostType {
+    username: string,
+    post: string
+}
 
 function App() {
-  return (
-    <div className="App">
-       <Navbar />
-    </div>
-  );
+    const user = useSelector((state: any) => state.user)
+    const dispatch = useDispatch()
+    const [posts, setPosts] = useState<IPostType[]>([])
+
+    useEffect(() => {
+        dispatch(updateUser())
+        fetch("/data/post.json")
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data)
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <Navbar/>
+            <Landing posts={posts}/>
+        </div>
+    );
 }
 
 export default App;
--- day35/src/components/Navbar.tsx
+++ day35/src/components/Navbar.tsx
@@ -1,34 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
 
-function Navbar() {
+const Navbar = () => {
+    const user = useSelector((state: any) => state.user)
+
     return (
-        <nav className="navbar navbar-expand-lg card">
-        <div className="container">
-            <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="navbar-brand" />
-          
-            <div className="search">
-                <input type="text" className="form-control border-1" placeholder="search" />
-            </div>
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#">
-                  <i className="bi bi-house-door-fill"></i>
-              </a>
-              <a className="nav-link" href="#">
-              <i className="bi bi-chat"></i>
-              </a>
-              <a className="nav-link" href="#">
-              <i className="bi bi-compass"></i>
-              </a>
-              <a className="nav-link" href="#">
-              <i className="bi bi-heart"></i>
-              </a>
-              <a className="nav-link" href="#">
-              <i className="bi bi-person-circle"></i>
-              </a>
-            </div>
-          </div>
-      </nav>
-    )
-}
+        <>
+            <nav className="navbar navbar-expand-lg card">
+                <div className="container">
+                    <a className="navbar-brand" href="/">
+                        <img src="/img/instagramlogo.png" alt=""/>
+                    </a>
+                    <div className="search">
+                        <input type="text" className="form-control" placeholder={"search"}/>
+                    </div>
 
-export default Navbar
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">
+                                    <i className="bi bi-house-door-fill"/>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">
+                                    <i className="bi bi-chat"/>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">
+                                    <i className="bi bi-compass"/>
+                                </a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">
+                                    <i className="bi bi-heart"/>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">
+                                    <img src={user.profilePicture} alt=""/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
